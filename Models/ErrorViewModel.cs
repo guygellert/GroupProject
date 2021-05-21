1b4e5fb5e8bfccf7806b40d2f6188deb48a079ff
Amit <amitlevi45@gmail.com>
2021-05-21 18:32:55 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Caveret.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
