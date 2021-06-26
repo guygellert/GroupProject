455d10cd5fd7eff90f379c8b4fbc083ff9953763
Amit <amitlevi45@gmail.com>
2021-06-26 10:16:51 +0300

    fix migrations part 2

--- Migrations/20210626065035_RebaseShops.Designer.cs
+++ Migrations/20210626065035_RebaseShops.Designer.cs
@@ -4,14 +4,16 @@ using Caveret.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Caveret.Migrations
 {
     [DbContext(typeof(CaveretContext))]
-    partial class CaveretContextModelSnapshot : ModelSnapshot
+    [Migration("20210626065035_RebaseShops")]
+    partial class RebaseShops
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
